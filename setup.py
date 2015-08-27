3824a6e8651726b2835fbaa5e4d5488b045e76cc
robnagler <github@q33.us>
2015-08-27 17:11:02 +0000

    working with numpy; old BuildFortranLib still in there

--- setup.py
+++ setup.py
@@ -11,11 +11,14 @@ import platform
 import subprocess
 import sys
 
-from distutils.command.build_clib import build_clib
+from numpy.distutils.command.build_clib import build_clib
+#from numpy.distutils.command.build_src import build_src
+#from distutils.command.build_clib import build_clib
 import pip
 import setuptools
 import setuptools.command.test
 import numpy
+from distutils.cmd import Command
 
 FORTRAN_LIB = 'shadow3c'
 
@@ -29,14 +32,39 @@ def main():
         tests_require=['pytest'],
         libraries=[
             (FORTRAN_LIB, {
-                'sources': ['c/shadow_bind_c.c'],
+                'sources': [
+                    'c/shadow_bind_c.c',
+                    'fortran/shadow_version.f90',
+                    'fortran/shadow_globaldefinitions.f90',
+                    'fortran/stringio.f90',
+                    'fortran/gfile.f90',
+                    'fortran/shadow_beamio.f90',
+                    'fortran/shadow_math.f90',
+                    'fortran/shadow_variables.f90',
+                    'fortran/shadow_roughness.f90',
+                    'fortran/shadow_kernel.f90',
+                    'fortran/shadow_synchrotron.f90',
+                    'fortran/shadow_pre_sync.f90',
+                    'fortran/shadow_pre_sync_urgent.f90',
+                    'fortran/shadow_preprocessors.f90',
+                    'fortran/shadow_postprocessors.f90',
+                    'fortran/shadow_bind_f.f90',
+                    'fortran/shadow_crl.f90',
+                ],
+                'macros': [('_COMPILE4NIX', 1)],
+                'include_dirs': ['def', 'fortran', 'c'],
+                'extra_compiler_args': [
+                    #'-ffree-line-length-none',
+                    #'-fomit-frame-pointer',
+                ],
             }),
         ],
         description='SHADOW is an open source ray tracing code for modeling optical systems.',
         long_description=_read('README.txt'),
         install_requires=_install_requires(),
         cmdclass={
-            'build_clib': BuildFortranLib,
+            'build_clib': BuildClib, ### BuildFortranLib,
+            'build_src': BuildSrc,
             'test': PyTest,
         },
         ext_modules=[
@@ -50,72 +78,23 @@ def main():
     )
 
 
-class BuildFortranLib(build_clib, object):
+class BuildSrc(Command, object):
 
-    def build_libraries(self, libraries, *args, **kwargs):
-        libraries = [x for x in libraries if x[0] != FORTRAN_LIB]
-        tmp_dir = self._libshadow3c_tmp_dir()
-        self._libshadow3c_version_h(tmp_dir)
-        obj = []
-        for src in (
-            'shadow_version.F90',
-            'shadow_globaldefinitions.F90',
-            'stringio.F90',
-            'gfile.F90',
-            'shadow_beamio.F90',
-            'shadow_math.F90',
-            'shadow_variables.F90',
-            'shadow_roughness.F90',
-            'shadow_kernel.F90',
-            'shadow_synchrotron.F90',
-            'shadow_pre_sync.F90',
-            'shadow_pre_sync_urgent.F90',
-            'shadow_preprocessors.F90',
-            'shadow_postprocessors.F90',
-            'shadow_bind_f.F90',
-            'shadow_crl.F90',
-            ):
-            self.distribution.announce('compiling {}'.format(src))
-            o = os.path.join(tmp_dir, os.path.splitext(src)[0]) + '.o'
-            obj.append(o)
-            subprocess.check_call([
-                'gfortran',
-                '-cpp',
-                '-fPIC',
-                '-ffree-line-length-none',
-                '-O2',
-                '-Ifortran',
-                '-Idef',
-                '-c',
-                '-fomit-frame-pointer',
-                '-J' + tmp_dir,
-                '-D_COMPILE4NIX',
-                '-o',
-                # Don't hardwire extension
-                o,
-                os.path.join('fortran', src),
-            ])
-        base = 'shadow_bind_c'
-        c = os.path.join('c', base + '.c')
-        self.distribution.announce('compiling {}'.format(c))
-        objects = self.compiler.compile(
-            [c],
-            output_dir=self.build_temp,
-            macros=[('_COMPILE4NIX',)],
-            include_dirs=['c', 'def'],
-            debug=self.debug)
-        obj.extend(objects)
-        self.distribution.announce('linking {}'.format(FORTRAN_LIB))
-        self.compiler.create_static_lib(
-            obj,
-            FORTRAN_LIB,
-            output_dir=self.build_clib,
-            debug=self.debug,
-        )
-        # Build the rest of the libraries (for completeness)
-        return super(BuildFortranLib, self).build_libraries(libraries, *args, **kwargs)
+    def initialize_options(*args, **kwargs):
+        pass
 
-    def _libshadow3c_version_h(self, tmp_dir):
+    def finalize_options(*args, **kwargs):
+        pass
+
+    def run(*args, **kwargs):
+        pass
+
+
+from pykern.pkdebug import pkdp
+
+class BuildClib(build_clib, object):
+
+    def _shadow3_version_h(self, tmp_dir):
         t = '''{hline}
 {header:^80}
 {hline}
@@ -151,16 +130,95 @@ class BuildFortranLib(build_clib, object):
                 line = ' '
             line = 'print *,"{}"\n'.format(line)
             lines.append(line)
-        with open(os.path.join(tmp_dir, 'shadow_version.h'), 'w') as f:
+        out = os.path.join(tmp_dir, 'shadow_version.h')
+        with open(out, 'w') as f:
             f.write(''.join(lines))
+        return out
 
-    def _libshadow3c_tmp_dir(self):
-        res = self.build_clib
-        try:
-            os.makedirs(res)
-        except OSError:
-            pass
-        return res
+    def build_libraries(self, *args, **kwargs):
+        self.mkpath(self.build_clib)
+        h = self._shadow3_version_h(self.build_clib)
+        f90 = self._f_compiler.compiler_f90
+        f90 = [x for x in f90 if x not in ['-Wall', '-fno-second-underscore']]
+        f90.extend(('-cpp', '-ffree-line-length-none', '-fomit-frame-pointer', '-I' + os.path.dirname(h)))
+        self._f_compiler.compiler_f90 = f90
+        return super(BuildClib, self).build_libraries(*args, **kwargs)
+
+
+class BuildFortranLib(build_clib, object):
+
+    def build_libraries(self, libraries, *args, **kwargs):
+        l = []
+        for x in libraries:
+            if x[0] == FORTRAN_LIB:
+                build_info = x[1]
+            else:
+                l.append(x)
+        libraries = l
+        tmp_dir = self.build_clib
+        self.mkpath(tmp_dir)
+        self._shadow3_version_h(tmp_dir)
+        obj = []
+        base = 'shadow_bind_c'
+        c = os.path.join('c', base + '.c')
+        self.distribution.announce('compiling {}'.format(c))
+        objects = self.compiler.compile(
+            [c],
+            output_dir=self.build_temp,
+            macros=[('_COMPILE4NIX',)],
+            include_dirs=['c', 'def'],
+            debug=self.debug)
+        obj.extend(objects)
+        obj.extend(self._shadow3_fortran(build_info, tmp_dir))
+        self.distribution.announce('linking {}'.format(FORTRAN_LIB))
+        self.compiler.create_static_lib(
+            obj,
+            FORTRAN_LIB,
+            output_dir=self.build_clib,
+            debug=self.debug,
+        )
+        # Build the rest of the libraries (for completeness)
+        return super(BuildFortranLib, self).build_libraries(libraries, *args, **kwargs)
+
+    def _shadow3_fortran(self, build_info, tmp_dir):
+        obj = []
+        for src in build_info['sources']:
+            if not src.endswith('f90'):
+                continue
+            self.distribution.announce('compiling {}'.format(src))
+            o = os.path.join(tmp_dir, os.path.splitext(src)[0]) + self.compiler.obj_extension
+            output_dir = os.path.dirname(o)
+            self.mkpath(output_dir)
+            obj.append(o)
+            subprocess.check_call([
+                'gfortran',
+                '-cpp',
+                '-fPIC',
+                '-ffree-line-length-none',
+                '-O2',
+                '-Ifortran',
+                '-Idef',
+                '-c',
+                '-fomit-frame-pointer',
+                '-J' + tmp_dir,
+                '-D_COMPILE4NIX',
+                '-o',
+                # Don't hardwire extension
+                o,
+                src,
+            ])
+        return obj
+
+    def _shadow3_object_filename(self, src, output_dir):
+        # disutils.ccompiler has object_filenames, but it
+        # asserts against the src_extensions, which are hardwired.
+        # Could look into "adding a compiler", but that is
+        # much more trouble than it is worth right now. It's
+        # too bad numpy.distutils is so complicated
+        assert not os.path.isabs(src), \
+            '{}: do not use absolute file names for sources'
+        obj = os.path.splitext(os.path.splitdrive(src)[1])[0]
+        return obj + self.compiler.obj_extension
 
 
 class PyTest(setuptools.command.test.test, object):
